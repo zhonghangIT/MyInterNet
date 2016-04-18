1b277c3013da914c911b9c486ceb9143865c18f8
ZhongHang <zhonghangIT@163.com>
2016-04-18 17:28:14 +0800

    更新了HttpClient的使用

--- app/src/main/java/com/uniquedu/myinternet/HttpClientActivity.java
+++ app/src/main/java/com/uniquedu/myinternet/HttpClientActivity.java
@@ -1,13 +1,41 @@
 package com.uniquedu.myinternet;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import cz.msebera.android.httpclient.HttpEntity;
+import cz.msebera.android.httpclient.HttpException;
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.HttpStatus;
+import cz.msebera.android.httpclient.NameValuePair;
+import cz.msebera.android.httpclient.ParseException;
+import cz.msebera.android.httpclient.StatusLine;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.config.RequestConfig;
+import cz.msebera.android.httpclient.client.entity.UrlEncodedFormEntity;
+import cz.msebera.android.httpclient.client.methods.HttpGet;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.entity.mime.MultipartEntityBuilder;
+import cz.msebera.android.httpclient.impl.client.HttpClients;
+import cz.msebera.android.httpclient.message.BasicNameValuePair;
+import cz.msebera.android.httpclient.util.EntityUtils;
 
 public class HttpClientActivity extends AppCompatActivity {
 
@@ -33,15 +61,175 @@ public class HttpClientActivity extends AppCompatActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button_get:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        doHttpClientGet("http://192.168.149.2:8080/MyJsonFileTest/StudentServlet?action=findlist");
+                    }
+                }).start();
                 break;
             case R.id.button_post:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        HashMap<String, String> params = new HashMap<String, String>();
+                        params.put("action", "findlist");
+                        doHttpClientPost("http://192.168.149.2:8080/MyJsonFileTest/StudentServlet", params);
+                    }
+                }).start();
+
                 break;
             case R.id.button_file:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        uploadFile("http://192.168.149.2:8080/MyJsonFileTest/UploadFile", Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/aa.png");
+                    }
+                }).start();
                 break;
             case R.id.button_down:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        download("http://192.168.149.2:8080/MyJsonFileTest/android/aa.png", Environment.getExternalStorageDirectory() + "/test.png");
+                    }
+                }).start();
                 break;
             case R.id.button_ssl:
                 break;
+        }
+    }
+
+    private void download(String url, String path) {
+        HttpClient client = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(url);
+        try {
+            HttpResponse response = client.execute(httpGet);
+            InputStream in = response.getEntity().getContent();
+            FileOutputStream out = new FileOutputStream(new File(path));
+            byte[] b = new byte[1024];
+            int len = 0;
+            while ((len = in.read(b)) != -1) {
+                out.write(b, 0, len);
+            }
+            in.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            httpGet.releaseConnection();
+        }
+        System.out.println("download, success!!");
+    }
+
+
+    /**
+     * @param url      上传文件网址
+     * @param filePath 文件的路径
+     */
+    private void uploadFile(String url, String filePath) {
+        //创建HttpClient客户端
+        HttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpPost httppost = new HttpPost(url);//创建提交的方法为post
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+            builder.addBinaryBody("file1", new File(filePath));
+            builder.addTextBody("normal", "normalfiled");
+            httppost.setEntity(builder.build());
+            HttpResponse response = httpclient.execute(httppost);
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode == HttpStatus.SC_OK) {
+                System.out.println("服务器正常响应.....");
+                HttpEntity resEntity = response.getEntity();
+                System.out.println(EntityUtils.toString(resEntity));//httpclient自带的工具类读取返回数据
+                System.out.println(resEntity.getContent());
+                EntityUtils.consume(resEntity);
+            }
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                httpclient.getConnectionManager().shutdown();
+            } catch (Exception ignore) {
+
+            }
+        }
+    }
+
+    private void doHttpClientGet(String url) {
+        StringBuffer buffer = null;
+        HttpClient client = HttpClients.createDefault();
+        HttpGet get = new HttpGet(url);
+        try {
+            get.setConfig(RequestConfig.DEFAULT);
+//            get.setHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            //执行get方法获得服务器返回的所有数据
+            HttpResponse response = client.execute(get);
+            //HttpClient获得服务器返回的表头。
+            StatusLine statusLine = response.getStatusLine();
+            //获得状态码
+            int code = statusLine.getStatusCode();
+            if (code == HttpURLConnection.HTTP_OK) {
+                //得到数据的实体。
+                HttpEntity entity = response.getEntity();
+                //得到数据的输入流。
+                InputStream is = entity.getContent();
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                String line = br.readLine();
+                buffer = new StringBuffer();
+                while (line != null) {
+                    System.out.println(line);
+                    buffer.append(line);
+                    line = br.readLine();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void doHttpClientPost(String url, HashMap<String, String> params) {
+        StringBuffer buffer = null;
+        HttpClient client = HttpClients.createDefault();
+        ;
+        HttpPost post = new HttpPost(url);
+        //设置传入的数据
+        ArrayList<NameValuePair> nameValuePairs = new ArrayList<>();
+        Iterator<String> iterator = params.keySet().iterator();
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            nameValuePairs.add(new BasicNameValuePair(key, params.get(key)));
+        }
+        try {
+            //设置传递的参数格式
+            post.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
+            post.setHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            //执行get方法获得服务器返回的所有数据
+            HttpResponse response = client.execute(post);
+            //HttpClient获得服务器返回的表头。
+            StatusLine statusLine = response.getStatusLine();
+            //获得状态码
+            int code = statusLine.getStatusCode();
+            if (code == HttpURLConnection.HTTP_OK) {
+                //得到数据的实体。
+                HttpEntity entity = response.getEntity();
+                //得到数据的输入流。
+                InputStream is = entity.getContent();
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                String line = br.readLine();
+                buffer = new StringBuffer();
+                while (line != null) {
+                    System.out.println(line);
+                    buffer.append(line);
+                    line = br.readLine();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- app/src/main/java/com/uniquedu/myinternet/VolleyActivity.java
+++ app/src/main/java/com/uniquedu/myinternet/VolleyActivity.java
@@ -1,13 +1,102 @@
 package com.uniquedu.myinternet;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class VolleyActivity extends AppCompatActivity {
+
+    @InjectView(R.id.button_get)
+    Button buttonGet;
+    @InjectView(R.id.button_post)
+    Button buttonPost;
+    @InjectView(R.id.button_file)
+    Button buttonFile;
+    @InjectView(R.id.button_down)
+    Button buttonDown;
+    @InjectView(R.id.button_ssl)
+    Button buttonSsl;
+    private RequestQueue queue;
+    private static final String TAG = "volleyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volley);
+        ButterKnife.inject(this);
+        queue = Volley.newRequestQueue(this);
+    }
+
+    @OnClick({R.id.button_get, R.id.button_post, R.id.button_file, R.id.button_down, R.id.button_ssl})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_get:
+                doGet("http://192.168.149.2:8080/MyJsonFileTest/StudentServlet?action=findlist");
+                break;
+            case R.id.button_post:
+                HashMap<String, String> params = new HashMap<>();
+                params.put("action", "findlist");
+                doPost("http://192.168.149.2:8080/MyJsonFileTest/StudentServlet", params);
+                break;
+            case R.id.button_file:
+                break;
+            case R.id.button_down:
+                break;
+            case R.id.button_ssl:
+                break;
+        }
+    }
+
+    private void doGet(String url) {
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "连接网络错误" + error.networkResponse.statusCode);
+            }
+        });
+        queue.add(request);
+    }
+
+    private void doPost(String url, final HashMap<String, String> params) {
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "连接网络错误" + error.networkResponse.statusCode);
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return params;
+            }
+        };
+        queue.add(request);
     }
 }
